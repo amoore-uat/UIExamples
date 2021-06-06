17d55225788104a9e10d826fd7eefb5811b762ad
Adam Moore <amoore@uat.edu>
2021-06-06 09:15:17 -0700

    Added MusicMixer and code for adjusting volume in the Options menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject optionsMenu;
+    public GameObject alertBox;
+    public AudioMixer mixer;
+    public Slider masterVolumeSlider;
 
     public void QuitGame()
     {
@@ -30,5 +35,75 @@ public class MainMenu : MonoBehaviour
     {
         // SceneManager.LoadScene("Level");
         SceneManager.LoadScene(1);
+    }
+
+    public void ChangeMasterVolume(float masterVolume)
+    {
+        mixer.SetFloat("MasterVolume", Mathf.Log10(masterVolume) * 20);
+    }
+
+    public void SaveOptions()
+    {
+        Debug.Log("Saving Options");
+        float mixerMasterVolume;
+        mixer.GetFloat("MasterVolume", out mixerMasterVolume);
+        PlayerPrefs.SetFloat("MasterVolume", mixerMasterVolume);
+        PlayerPrefs.SetFloat("MasterVolumeSlider", masterVolumeSlider.value);
+    }
+
+    public void LoadOptions()
+    {
+        Debug.Log("Loading Options");
+        float mixerMasterVolume = PlayerPrefs.GetFloat("MasterVolume", 0f);
+        mixer.SetFloat("MasterVolume", mixerMasterVolume);
+        float masterSliderValue = PlayerPrefs.GetFloat("MasterVolumeSlider", 1f);
+        masterVolumeSlider.value = masterSliderValue;
+    }
+
+    private void Start()
+    {
+        LoadOptions();
+    }
+
+    public void CheckForChanges()
+    {
+        float savedMasterVolume = PlayerPrefs.GetFloat("MasterVolume", 0f);
+        float actualMasterVolume;
+        mixer.GetFloat("MasterVolume", out actualMasterVolume);
+
+        // TODO: Actually set this information up in code and in editor.
+        float savedMusicVolume = PlayerPrefs.GetFloat("MusicVolume", 0f);
+        float actualMusicVolume;
+        mixer.GetFloat("MusicVolume", out actualMusicVolume);
+
+        // TODO: Actually set this information up in code and in editor.
+        float savedSFXVolume = PlayerPrefs.GetFloat("SFXVolume", 0f);
+        float actualSFXVolume;
+        mixer.GetFloat("SFXVolume", out actualSFXVolume);
+
+        if (Mathf.Approximately(savedMasterVolume, actualMasterVolume) &&
+            Mathf.Approximately(savedMusicVolume, actualMusicVolume) &&
+            Mathf.Approximately(savedSFXVolume, actualSFXVolume))
+        {
+            // The values are the same
+            CloseOptionsMenu();
+        }
+        else
+        {
+            // The values are different
+            ShowAlertBox();
+        }
+    }
+
+    public void ShowAlertBox()
+    {
+        optionsMenu.SetActive(false);
+        alertBox.SetActive(true);
+    }
+
+    public void CloseAlertBox()
+    {
+        mainMenu.SetActive(true);
+        alertBox.SetActive(false);
     }
 }
